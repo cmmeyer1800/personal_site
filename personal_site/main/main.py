e5fe79844358f1e213d982adaa8f352418e1b38a
Collin Meyer <collinmmeyer@gmail.com>
2022-06-10 10:05:36 -0500

    added projects page

--- personal_site/main/main.py
+++ personal_site/main/main.py
@@ -4,6 +4,11 @@ main_bp = Blueprint('main_bp', __name__,
     template_folder='templates',
     static_folder='static', static_url_path='assets')
 
-@main_bp.route("/")
+@main_bp.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
+
+
+@main_bp.route("/projects", methods=["GET"])
+def projects():
+    return render_template("projects.html")
